4a27d5e20072ac1ac47386ac79246970e985157c
VIctor Law <manhon95@gmail.com>
2023-06-05 17:12:36 +0800

    [gpt-server] update trip route

--- gpt-server/src/gpt/gpt.controller.ts
+++ gpt-server/src/gpt/gpt.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { GPTService } from './gpt.service';
 import { GPTRequestDto } from './dto/gpt-request.dto';
+import { GPTTripRequestDto } from './dto/gpt-trip-request.dto';
 
 @Controller('gpt')
 export class GPTController {
@@ -9,5 +10,12 @@ export class GPTController {
   @Get()
   async getGPTResponse(@Body() gptRequestDto: GPTRequestDto): Promise<string> {
     return await this.gptService.getGPTResponse(gptRequestDto);
+  }
+
+  @Get('trip')
+  async getGPTTripResponse(
+    @Body() gptTripRequestDto: GPTTripRequestDto,
+  ): Promise<string> {
+    return await this.gptService.getGPTTripResponse(gptTripRequestDto);
   }
 }
