96155b0be85e5fb446d1b6703fa581cbc4754dc5
VIctor Law <manhon95@gmail.com>
2023-06-16 19:03:15 +0800

    [GPT server] change route post

--- gpt-server/src/app.controller.ts
+++ gpt-server/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/123')
+@Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
