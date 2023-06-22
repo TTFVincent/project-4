9fefe08b39f53de7a1e24529a073bb7d1dfe89a5
VIctor Law <manhon95@gmail.com>
2023-06-22 16:19:30 +0800

    [backend] triplocation entity table update

--- nest-backend/src/models/trip-locations/entities/trip-location.entity.ts
+++ nest-backend/src/models/trip-locations/entities/trip-location.entity.ts
@@ -8,9 +8,19 @@ import {
   Model,
   Table,
 } from 'sequelize-typescript';
+import { Trip } from 'src/models/trips/entities/trip.entity';
+import { Location } from 'src/models/locations/entities/location.entity';
 
-Table({ tableName: 'trip_locations' });
+@Table({ tableName: 'trip_locations' })
 export class TripLocation extends Model {
   @Column({ primaryKey: true, autoIncrement: true })
   id: number;
+
+  @ForeignKey(() => Trip)
+  @Column
+  trip_id: number;
+
+  @ForeignKey(() => Location)
+  @Column
+  location_id: number;
 }
