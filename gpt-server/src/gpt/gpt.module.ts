e17c7fb5ed3d3c878c126331b43a9e4213add968
VIctor Law <manhon95@gmail.com>
2023-06-22 16:59:06 +0800

    [gpt server] bugfix

--- gpt-server/src/gpt/gpt.module.ts
+++ gpt-server/src/gpt/gpt.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GPTController } from './gpt.controller';
 import { GPTService } from './gpt.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { GPTRequest } from './entities/gptRequest';
 
 @Module({
+  imports: [SequelizeModule.forFeature([GPTRequest])],
   controllers: [GPTController],
   providers: [GPTService],
 })
