bab56f5d6c64a104a4ccc17f1a5b0d22219cd833
Vincent Tse <king2003man2@gmail.com>
2023-06-24 22:17:09 +0800

    sync

--- gpt-server/src/gpt/gpt.service.ts
+++ gpt-server/src/gpt/gpt.service.ts
@@ -48,10 +48,18 @@ export class GPTService {
         messages: [
           {
             role: 'user',
-            content: `Generate a personalized travel itinerary for a trip to ${gptTripRequestDto.destination} with a budget of HKD ${gptTripRequestDto.budget}. The traveler is interested in a ${gptTripRequestDto.travel_style} vacation and enjoys ${gptTripRequestDto.interests_new}. The itinerary should include ${gptTripRequestDto.activity_type} activities and ${gptTripRequestDto.cuisine_type} dining options. Please provide a detailed itinerary with daily recommendations from ${gptTripRequestDto.start_time} to ${gptTripRequestDto.end_time}, including suggested destinations, activities, and dining options. The resulting JSON object should be in this format without other text: [{\"location\":,\"location_address\":,\"latitude\":,\"longitude\":,\"from_time\":,\"to_time\":,\"budget\":},]. The JSON object:`,
+            // content: `Generate a personalized travel itinerary for a trip to ${gptTripRequestDto.destination} with a budget of HKD ${gptTripRequestDto.budget}. The traveler is interested in a ${gptTripRequestDto.travel_style} vacation and enjoys ${gptTripRequestDto.interests_new}. The itinerary should include ${gptTripRequestDto.activity_type} activities and ${gptTripRequestDto.cuisine_type} dining options. Please provide a detailed itinerary with daily recommendations from ${gptTripRequestDto.start_time} to ${gptTripRequestDto.end_time}, including suggested destinations, activities, and dining options. The resulting JSON object should be in this format without other text: [{\"location\":,\"location_address\":,\"latitude\":,\"longitude\":,\"from_time\":,\"to_time\":,\"budget\":},]. The JSON object:`,
+            content: `Generate a personalized travel itinerary for a trip in the location of ${gptTripRequestDto.destination} with a budget of HKD ${gptTripRequestDto.budget}.
+            The activities choices  ${gptTripRequestDto.travel_style} and the trip activities should focus more on ${gptTripRequestDto.interests_new} if possible.
+            Please provide a detailed itinerary with daily recommendations from ${gptTripRequestDto.start_time} to ${gptTripRequestDto.end_time},
+            if ${gptTripRequestDto.start_time} before 12:00pm suggest a restaurant with ${gptTripRequestDto.cuisine_type} near by the last activity location,
+            and if ${gptTripRequestDto.end_time} before 07:00pm suggest a restaurant with ${gptTripRequestDto.cuisine_type} for dinner near by the last activity location.
+            the suggested activities must not over lap with other activities, also spare some time for traveling to the next location according to th distance.
+            The resulting JSON object must be in this format only and must not have other string:
+            [{\"location\":,\"location_address\":,\"latitude\":,\"longitude\":,\"from_time\":,\"to_time\":,\"budget\":},]. The JSON object:`,
           },
         ],
-        temperature: 0.5,
+        temperature: 0.8,
         max_tokens: 2047,
       });
       const output = response.data.choices[0].message?.content;
